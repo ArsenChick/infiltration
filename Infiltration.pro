2234da0204b491b9128971be11962bd1fd8314ff
Arseniy <arseniy1304@gmail.com>
2020-06-11 16:24:55 +0300

    ver 0.3.0 - added camera and enemy movement

---
+++ Infiltration.pro
@@ -0,0 +1,26 @@
+LIBS += -L$$PWD/external/SFML-2.5.1/lib
+
+TEMPLATE = app
+CONFIG += gui
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+        enemy.cpp \
+        hero.cpp \
+        main.cpp \
+        map.cpp \
+        mapgenerator.cpp
+
+CONFIG(release, debug|release): LIBS += -lsfml-audio -lsfml-graphics -lsfml-main -lsfml-network -lsfml-window -lsfml-system
+CONFIG(debug, debug|release): LIBS += -lsfml-audio-d -lsfml-graphics-d -lsfml-main-d -lsfml-network-d -lsfml-window-d -lsfml-system-d
+
+INCLUDEPATH += $$PWD/external/SFML-2.5.1/include
+DEPENDPATH += $$PWD/external/SFML-2.5.1/include
+
+HEADERS += \
+    defines.h \
+    enemy.h \
+    hero.h \
+    map.h \
+    mapgenerator.h
---
+++ enemy.cpp
@@ -0,0 +1,116 @@
+#include "enemy.h"
+#include <algorithm>
+
+Enemy::Enemy()
+{
+    speed = 0.1;
+    look = UP;
+}
+
+void Enemy::setStartPosition(int tileno)
+{
+    pos = tileno;
+    int row = tileno / LWIDTH;
+    int col = tileno % LWIDTH;
+
+    x = col * TILESIZE + (float)TILESIZE / 2 - (float)ENEMYW / 2;
+    y = row * TILESIZE + (float)TILESIZE / 2 - (float)ENEMYH / 2;
+    sprite.setPosition(x, y);
+
+    gen.seed(rand() % (int)x + y);
+}
+
+void Enemy::load(sf::Texture *enemy_texture)
+{
+    texture = enemy_texture;
+    sprite.setTexture(*texture);
+    sprite.setTextureRect(sf::IntRect(0, 0, ENEMYW, ENEMYH));
+}
+
+void Enemy::move(std::vector<int>& level, float time)
+{
+    if (time_w != -1) {
+        if (clock_e.getElapsedTime().asSeconds() - time_w >= 0.5) {
+            clock_e.restart();
+            time_w = -1;
+            look = gen() % 5;
+        }
+        return;
+    }
+
+    float last_x = x;
+    float last_y = y;
+
+    int row = pos / LWIDTH;
+    int col = pos % LWIDTH;
+
+    float for_y = row * TILESIZE + (float)(TILESIZE - ENEMYH) / 2;
+    float for_x = col * TILESIZE + (float)(TILESIZE - ENEMYW) / 2;
+
+    float left_bound = col * TILESIZE + WALLSIZE;
+    float upper_bound = row * TILESIZE + WALLSIZE;
+    float right_bound = (col+1) * TILESIZE - WALLSIZE - ENEMYW;
+    float lower_bound = (row+1) * TILESIZE - WALLSIZE - ENEMYH;
+
+    if (look == LEFT) {
+        if ((level[pos] & ROUTELEFT) != 0) {
+            if (y >= upper_bound && y < lower_bound) {
+                y = for_y;
+                sprite.setPosition(x, y);
+                sprite.move(-speed*time, 0);
+            }
+            //else if (x - speed*time >= for_x)
+            //sprite.move(-speed*time, 0);
+        }
+        else if (x - speed*time >= for_x)
+            sprite.move(-speed*time, 0);
+    }
+    if (look == RIGHT) {
+        if ((level[pos] & ROUTERIGHT) != 0) {
+            if (y >= upper_bound && y < lower_bound) {
+                y = for_y;
+                sprite.setPosition(x, y);
+                sprite.move(speed*time, 0);
+            }
+            //else if (x + speed*time < for_x)
+            //sprite.move(speed*time, 0);
+        }
+        else if (x + speed*time < for_x)
+            sprite.move(speed*time, 0);
+    }
+    if (look == UP) {
+        if ((level[pos] & ROUTEUP) != 0) {
+            if (x >= left_bound && x < right_bound) {
+                x = for_x;
+                sprite.setPosition(x, y);
+                sprite.move(0, -speed*time);
+            }
+            //else if (y - speed*time >= for_y)
+            //sprite.move(0, -speed*time);
+        }
+        else if (y - speed*time >= for_y)
+            sprite.move(0, -speed*time);
+    }
+    if (look == DOWN) {
+        if ((level[pos] & ROUTEDOWN) != 0) {
+            if (x >= left_bound && x < right_bound) {
+                x = for_x;
+                sprite.setPosition(x, y);
+                sprite.move(0, speed*time);
+            }
+            //else if (y + speed*time < for_y)
+            //sprite.move(0, speed*time);
+        }
+        else if (y + speed*time < for_y)
+            sprite.move(0, speed*time);
+    }
+
+    x = sprite.getPosition().x;
+    y = sprite.getPosition().y;
+
+    pos = int(x)/TILESIZE + int(y)/TILESIZE * LWIDTH;
+
+    if ((last_x == x) && (last_y == y)) {
+        time_w = clock_e.getElapsedTime().asSeconds();
+    }
+}
